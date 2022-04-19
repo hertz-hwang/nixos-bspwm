4c2fc78fb5bddd12af9f7f19eab9b61b35b2e8e6
Jaus_Hwang <jaus_hwang@88.com>
2022-04-19 08:32:23 +0800

    Tue Apr 19 08:32:23 AM CST 2022

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
     };
   };
 
-  outputs = inputs @ { self, nixpkgs, home-manager, ... }:
+  outputs = inputs @ { self, nixpkgs, home-manager, musnix ... }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
